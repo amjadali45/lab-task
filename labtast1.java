ee04afa9c012a8b5037de0cc9769762f3c73befe
amjadali45 <117860732+amjadali45@users.noreply.github.com>
2022-11-09 11:51:53 -0800

    Add files via upload

---
+++ labtast1.java
@@ -0,0 +1,15 @@
+ public class labtast1 {
+    public static void main(String[] args) {
+        int mySetNo = 15;              
+        float myNumber = 0324.27f;    
+        char myLetter = 'A';         
+        boolean mybool = true;       
+        String myText = "My Name Is Amjad Ali
+        System.out.println(mySetNo);
+        System.out.println(myNumber);
+        System.out.println(myLetter);
+        System.out.println(mybool);
+        System.out.println(myText);
+    }
+}
+
